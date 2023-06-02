9fc8d31631bd0be49adc0cf523f05f3470a30d1b
j.schmidt <j.schmidt@LP77-JSC>
2023-06-02 08:45:52 +0200

    Projektdateien hinzufügen.

---
+++ APM Angebotstool/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace APM_Angebotstool
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductGroupComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBoxItem selectedItem = (ComboBoxItem)ProductGroupComboBox.SelectedItem;
+            string selectedGroup = selectedItem.Content.ToString();
+
+            // Hier können Sie die Produkte für die ausgewählte Produktgruppe festlegen
+            // Beispiel:
+            ProductComboBox.Items.Clear();
+            if (selectedGroup == "Produktgruppe 1")
+            {
+                ProductComboBox.Items.Add("Produkt 1.1");
+                ProductComboBox.Items.Add("Produkt 1.2");
+                ProductComboBox.Items.Add("Produkt 1.3");
+            }
+            else if (selectedGroup == "Produktgruppe 2")
+            {
+                ProductComboBox.Items.Add("Produkt 2.1");
+                ProductComboBox.Items.Add("Produkt 2.2");
+                ProductComboBox.Items.Add("Produkt 2.3");
+            }
+            else if (selectedGroup == "Produktgruppe 3")
+            {
+                ProductComboBox.Items.Add("Produkt 3.1");
+                ProductComboBox.Items.Add("Produkt 3.2");
+                ProductComboBox.Items.Add("Produkt 3.3");
+            }
+
+            //ProductSelectionPanel.Visibility = Visibility.Visible;
+        }
+    }
+}
